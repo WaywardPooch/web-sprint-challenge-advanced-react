5f346585f521d60da6528d70f8c45079672f9a3a
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-01 05:55:53 -0700

    added axios request for retrieving plant list data from API

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -4,11 +4,24 @@ import axios from "axios";
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
   state = {
-    plants:[]
-  }
+    plants: [],
+  };
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
+  async componentDidMount() {
+    try {
+      const plantData = await axios.get("http://localhost:3333/plants");
+      this.setState({
+        ...this.state,
+        plants: plantData.data
+      });
+    } catch (error) {
+      console.error("[AXIOS] Could not get plant data!", error);
+    } finally {
+      console.log("[AXIOS] Request Attempted!");
+    }
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
