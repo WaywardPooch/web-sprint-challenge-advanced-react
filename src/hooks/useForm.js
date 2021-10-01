b81d9fcb9d66a2a012170a222e028bffb8ec4eca
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-01 06:28:54 -0700

    added a useForm() hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,32 @@
 // write your custom hook here to control your checkout form
+
+// ========== IMPORTS
+import { useState } from "react";
+
+// ========== DEFINITIONS
+const useForm = (initialFormValues) => {
+  // ========== STATES
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  // ========== EVENT HANDLERS
+  const handleFormUpdate = (event) => {
+    // When called, update formValues with new values
+    setFormValues({
+      ...formValues,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleFormSubmit = (event) => {
+    // Prevent page refresh
+    event.preventDefault();
+    // Allow showing success message
+    setShowSuccessMessage(true);
+  };
+
+  // ========== OUTPUT
+  return [formValues, showSuccessMessage, handleFormUpdate, handleFormSubmit];
+};
+
+// ========== EXPORTS
+export default useForm;
